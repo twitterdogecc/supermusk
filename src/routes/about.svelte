d39d8052942c4a3e1d2486cc876387366f05595b
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-04 14:09:33 +0800

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,51 +4,51 @@
 
 <h1>About Twitter Doge</h1>
 
-<p><strong>⼀、Originally</strong></p>
+<p><strong>1.Originally</strong></p>
 <p>In honor of Musk, the $44 billion wholly-owned acquisition of Twitter, Musk launched TwitterDoge</p>
 
-<p><strong>⼆、Vision</strong></p>
+<p><strong>2.Vision</strong></p>
 <p>Let the faith of decentralization be great again, lead the way, be a white-robed man, innovate value, create a DAO node economic community, and let everyone who loves the blockchain in the world be a business
 </p>
 
-<p><strong>三、Ecological Matrix</strong></p>
+<p><strong>3.Ecological Matrix</strong></p>
 <p>Wallet, K-Line, Decentralized Exchange, Cross-chain Storage Expansion, Unbounded Payment, NFT, Metaverse</p>
 
-<p><strong>四、Ecological Extension</strong></p>
+<p><strong>4.Ecological Extension</strong></p>
 <p>web3.0, Co-construction of Chainyou Universe, Digital Business School, Hong Kong Investment Fund, Global Business Restart Plan</p>
 
-<p><strong>五、Security Consensus</strong></p>
+<p><strong>5.Security Consensus</strong></p>
 <p>Tokens are divided into Doge, the pot is locked, and all permissions are discarded</p>
 
-<p><strong>六、Total Amount of Tokens</strong></p>
+<p><strong>6.Total Amount of Tokens</strong></p>
 <p>The total amount is 100 billion. Before the launch, the black hole will destroy 90 billion, and the circulation will be 10 billion.</p>
 
-<p><strong>七、Token Distribution</strong></p>
+<p><strong>7.Token Distribution</strong></p>
 <p>ido 4 billion (40%), Lp liquidity 4 billion (40%), ecological fund 1 billion (10%), 3.0 pot 1 billion (10%)</p>
 
-<p><strong>八、Ecological Fund</strong></p>
+<p><strong>8.Ecological Fund</strong></p>
 <p>Project marketing and ecology, national DAO, foundation 10% permanent contract lock and only share dividends for ecological construction and marketing</p>
 
-<p><strong>九、Transaction Mechanism</strong></p>
+<p><strong>9.Transaction Mechanism</strong></p>
 <p>Buy 12% Sell 12% Hold 10,000 coins to enjoy DOGE dividends</p>
 <p>8％ Violent dividend Doge</p>
 <p>2％ Repurchase and destroy</p>
 <p>2％ Lp reflow</p>
 
-<p><strong>十、Twitter3.0</strong></p>
+<p><strong>10.Twitter3.0</strong></p>
 <p>Node NFT exclusive platform A total of 100,000 pieces <100,000 pieces></p>
 <p>40％ Mapping</p>
 <p>40％ Lp liquidity</p>
 <p>10％ Fund</p>
 <p>10％ NFT mining</p>
 
-<p><strong>十一、Transaction Mechanism</strong></p>
+<p><strong>11.Transaction Mechanism</strong></p>
 <p>buy5％ sell5％</p>
 <p>3％ Reflux twitterDoge into a black hole</p>
 <p>1％ Black hole</p>
 <p>1％ Marketing</p>
 
-<p><strong>十二、NFT Permissions</strong></p>
+<p><strong>12.NFT Permissions</strong></p>
 <p>Total 300</p>
 <p>Mining Twitter3.0 10,000 pieces, mined in 100 days</p>
 <p>All ecological 50% weighted dividends</p>

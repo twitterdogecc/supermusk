96ee951491a9c422d533b9c0d0009baa9ea77a89
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-04 13:57:04 +0800

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,50 +4,51 @@
 
 <h1>About Twitter Doge</h1>
 
-<p><strong>⼀、Originally 起源</strong></p>
-<p>为了纪念马斯克，440亿美金全资收购推特，马斯克推出 TwitterDoge</p>
+<p><strong>⼀、Originally</strong></p>
+<p>In honor of Musk, the $44 billion wholly-owned acquisition of Twitter, Musk launched TwitterDoge</p>
 
-<p><strong>⼆、Vision 愿景</strong></p>
-<p>让去中⼼化的信仰再次伟⼤，领路者⽩袍⽢道夫，创新价值，创⽴DAO节点经济共同体，让全球区块链爱号者人人做庄</p>
+<p><strong>⼆、Vision</strong></p>
+<p>Let the faith of decentralization be great again, lead the way, be a white-robed man, innovate value, create a DAO node economic community, and let everyone who loves the blockchain in the world be a business
+</p>
 
-<p><strong>三、Ecological Matrix ⽣态矩阵</strong></p>
-<p>钱包、k线、去中⼼化交易所、跨链拓展储存、⽆界支付、NFT、元宇宙</p>
+<p><strong>三、Ecological Matrix</strong></p>
+<p>Wallet, K-Line, Decentralized Exchange, Cross-chain Storage Expansion, Unbounded Payment, NFT, Metaverse</p>
 
-<p><strong>四、Ecological Extension ⽣态延伸</strong></p>
-<p>web3.0、共建链游宇宙、数字商学院、⻛投基⾦、全球商业重启计划</p>
+<p><strong>四、Ecological Extension</strong></p>
+<p>web3.0, Co-construction of Chainyou Universe, Digital Business School, Hong Kong Investment Fund, Global Business Restart Plan</p>
 
-<p><strong>五、Security Consensus 安全共识</strong></p>
-<p>持币分Doge、底池上锁、所有权限丢弃</p>
+<p><strong>五、Security Consensus</strong></p>
+<p>Tokens are divided into Doge, the pot is locked, and all permissions are discarded</p>
 
-<p><strong>六、Total Amount of Tokens 代币总量</strong></p>
-<p>总量1000亿，上线之前黑洞销毁900亿，流通100亿</p>
+<p><strong>六、Total Amount of Tokens</strong></p>
+<p>The total amount is 100 billion. Before the launch, the black hole will destroy 90 billion, and the circulation will be 10 billion.</p>
 
-<p><strong>七、Token Distribution 代币分配</strong></p>
-<p>ido 40亿(40％)，Lp流动性40亿(40％)，生态基金10亿(10％)，3.0底池10亿(10%)</p>
+<p><strong>七、Token Distribution</strong></p>
+<p>ido 4 billion (40%), Lp liquidity 4 billion (40%), ecological fund 1 billion (10%), 3.0 pot 1 billion (10%)</p>
 
-<p><strong>八、Ecological Fund 生态基金</strong></p>
-<p>项目营销以及生态，全民DAO，基金会10％永久合约锁死只享分红，用于生态建设，以及营销</p>
+<p><strong>八、Ecological Fund</strong></p>
+<p>Project marketing and ecology, national DAO, foundation 10% permanent contract lock and only share dividends for ecological construction and marketing</p>
 
-<p><strong>九、Transaction Mechanism 交易机制</strong></p>
-<p>买12％卖12％ 持币10000枚享受DOGE分红</p>
-<p>8％ 暴力分红Doge</p>
-<p>2％ 回购销毁</p>
-<p>2％ Lp回流</p>
+<p><strong>九、Transaction Mechanism</strong></p>
+<p>Buy 12% Sell 12% Hold 10,000 coins to enjoy DOGE dividends</p>
+<p>8％ Violent dividend Doge</p>
+<p>2％ Repurchase and destroy</p>
+<p>2％ Lp reflow</p>
 
 <p><strong>十、Twitter3.0</strong></p>
-<p>节点NFT专属平台 总量100000枚〈10万枚〉</p>
-<p>40％ 映射</p>
-<p>40％ Lp流动性</p>
-<p>10％ 基金</p>
-<p>10％ NFT挖矿</p>
+<p>Node NFT exclusive platform A total of 100,000 pieces <100,000 pieces></p>
+<p>40％ Mapping</p>
+<p>40％ Lp liquidity</p>
+<p>10％ Fund</p>
+<p>10％ NFT mining</p>
 
-<p><strong>十一、Transaction Mechanism 交易机制</strong></p>
-<p>买5％ 卖5％</p>
-<p>3％ 回流twitterDoge进入黑洞</p>
-<p>1％ 黑洞</p>
-<p>1％ 营销</p>
+<p><strong>十一、Transaction Mechanism</strong></p>
+<p>buy5％ sell5％</p>
+<p>3％ Reflux twitterDoge into a black hole</p>
+<p>1％ Black hole</p>
+<p>1％ Marketing</p>
 
-<p><strong>十二、NFT Permissions NFT权限</strong></p>
-<p>总量300</p>
-<p>挖矿Twitter3.0 1万枚 100天挖完</p>
-<p>所有生态50％加权分红</p>
+<p><strong>十二、NFT Permissions</strong></p>
+<p>Total 300</p>
+<p>Mining Twitter3.0 10,000 pieces, mined in 100 days</p>
+<p>All ecological 50% weighted dividends</p>

5020fe71beeb64b2910a91ba144c8c97e582201b
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-04 11:49:26 +0800

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,8 @@
 <svelte:head>
-	<title>About</title>
+	<title>Twitter Doge</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>About Twitter Doge</h1>
 
 <p>⼀、起源</p>
 <p>为了纪念马斯克，440亿美金全资收购推特，马斯克推出 TwitterDoge</p>

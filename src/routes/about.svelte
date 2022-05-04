6683bc17d1835be86ed4ca73836657609c55de68
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-04 11:58:06 +0800

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,50 +4,50 @@
 
 <h1>About Twitter Doge</h1>
 
-<p><strong>⼀、起源</strong></p>
+<p><strong>⼀、Originally 起源</strong></p>
 <p>为了纪念马斯克，440亿美金全资收购推特，马斯克推出 TwitterDoge</p>
 
-<p><strong>⼆、愿景</strong></p>
+<p><strong>⼆、Vision 愿景</strong></p>
 <p>让去中⼼化的信仰再次伟⼤，领路者⽩袍⽢道夫，创新价值，创⽴DAO节点经济共同体，让全球区块链爱号者人人做庄</p>
 
-<p><strong>三、⽣态矩阵</strong></p>
+<p><strong>三、Ecological Matrix ⽣态矩阵</strong></p>
 <p>钱包、k线、去中⼼化交易所、跨链拓展储存、⽆界支付、NFT、元宇宙</p>
 
-<p><strong>四、⽣态延伸</strong></p>
+<p><strong>四、Ecological Extension ⽣态延伸</strong></p>
 <p>web3.0、共建链游宇宙、数字商学院、⻛投基⾦、全球商业重启计划</p>
 
-<p><strong>五、安全共识</strong></p>
+<p><strong>五、Security Consensus 安全共识</strong></p>
 <p>持币分Doge、底池上锁、所有权限丢弃</p>
 
-<p><strong>六、代币总量</strong></p>
+<p><strong>六、Total Amount of Tokens 代币总量</strong></p>
 <p>总量1000亿，上线之前黑洞销毁900亿，流通100亿</p>
 
-<p><strong>七、代币分配</strong></p>
+<p><strong>七、Token Distribution 代币分配</strong></p>
 <p>ido 40亿(40％)，Lp流动性40亿(40％)，生态基金10亿(10％)，3.0底池10亿(10%)</p>
 
-<p><strong>八、生态基金</strong></p>
+<p><strong>八、Ecological Fund 生态基金</strong></p>
 <p>项目营销以及生态，全民DAO，基金会10％永久合约锁死只享分红，用于生态建设，以及营销</p>
 
-<p><strong>九、交易机制</strong></p>
+<p><strong>九、Transaction Mechanism 交易机制</strong></p>
 <p>买12％卖12％ 持币10000枚享受DOGE分红</p>
 <p>8％ 暴力分红Doge</p>
 <p>2％ 回购销毁</p>
 <p>2％ Lp回流</p>
 
-<p><strong>十、TWitter3.0</strong></p>
+<p><strong>十、Twitter3.0</strong></p>
 <p>节点NFT专属平台 总量100000枚〈10万枚〉</p>
 <p>40％ 映射</p>
 <p>40％ Lp流动性</p>
 <p>10％ 基金</p>
 <p>10％ NFT挖矿</p>
 
-<p><strong>十一、交易机制</strong></p>
+<p><strong>十一、Transaction Mechanism 交易机制</strong></p>
 <p>买5％ 卖5％</p>
 <p>3％ 回流twitterDoge进入黑洞</p>
 <p>1％ 黑洞</p>
 <p>1％ 营销</p>
 
-<p><strong>十二、NFT权限</strong></p>
+<p><strong>十二、NFT Permissions NFT权限</strong></p>
 <p>总量300</p>
 <p>挖矿Twitter3.0 1万枚 100天挖完</p>
 <p>所有生态50％加权分红</p>

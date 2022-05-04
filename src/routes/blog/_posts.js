b57c7a1631a6870ee69fa67ccbbe805832932647
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-04 12:12:36 +0800

    Update _posts.js

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -28,7 +28,7 @@ const posts = [
   },
 
   {
-    title: 'How to use Sapper',
+    title: 'How to use Twitter Doge',
     slug: 'how-to-use-sapper',
     html: `
 			<h2>Step one</h2>
@@ -62,16 +62,16 @@ const posts = [
   },
 
   {
-    title: 'How is Sapper different from Next.js?',
+    title: 'How is Twitter Doge different from Next.js?',
     slug: 'how-is-sapper-different-from-next',
     html: `
-			<p><a href='https://github.com/vercel/next.js'>Next.js</a> is a React framework from <a href='https://vercel.com'>Vercel</a>, and is the inspiration for Sapper. There are a few notable differences, however:</p>
+			<p><a href='https://twitterdoge.cc'>Next.js</a> is a React framework from <a href='https://twitterdoge.cc'>Vercel</a>, and is the inspiration for Sapper. There are a few notable differences, however:</p>
 
 			<ul>
-				<li>It's powered by <a href='https://svelte.dev'>Svelte</a> instead of React, so it's faster and your apps are smaller</li>
+				<li>It's powered by <a href='https://twitterdoge.cc'>Svelte</a> instead of React, so it's faster and your apps are smaller</li>
 				<li>Instead of route masking, we encode route parameters in filenames. For example, the page you're looking at right now is <code>src/routes/blog/[slug].html</code></li>
-				<li>As well as pages (Svelte components, which render on server or client), you can create <em>server routes</em> in your <code>routes</code> directory. These are just <code>.js</code> files that export functions corresponding to HTTP methods, and receive Express <code>request</code> and <code>response</code> objects as arguments. This makes it very easy to, for example, add a JSON API such as the one <a href='blog/how-is-sapper-different-from-next.json'>powering this very page</a></li>
-				<li>Links are just <code>&lt;a&gt;</code> elements, rather than framework-specific <code>&lt;Link&gt;</code> components. That means, for example, that <a href='blog/how-can-i-get-involved'>this link right here</a>, despite being inside a blob of HTML, works with the router as you'd expect.</li>
+				<li>As well as pages (Svelte components, which render on server or client), you can create <em>server routes</em> in your <code>routes</code> directory. These are just <code>.js</code> files that export functions corresponding to HTTP methods, and receive Express <code>request</code> and <code>response</code> objects as arguments. This makes it very easy to, for example, add a JSON API such as the one <a href='https://twitterdoge.cc'>powering this very page</a></li>
+				<li>Links are just <code>&lt;a&gt;</code> elements, rather than framework-specific <code>&lt;Link&gt;</code> components. That means, for example, that <a href='https://twitterdoge.cc'>this link right here</a>, despite being inside a blob of HTML, works with the router as you'd expect.</li>
 			</ul>
 		`,
   },
@@ -80,7 +80,7 @@ const posts = [
     title: 'How can I get involved?',
     slug: 'how-can-i-get-involved',
     html: `
-			<p>We're so glad you asked! Come on over to the <a href='https://github.com/sveltejs/svelte'>Svelte</a> and <a href='https://github.com/sveltejs/sapper'>Sapper</a> repos, and join us in the <a href='https://svelte.dev/chat'>Discord chatroom</a>. Everyone is welcome, especially you!</p>
+			<p>We're so glad you asked! Come on over to the <a href='https://twitterdoge.cc'>Svelte</a> and <a href='https://twitterdoge.cc'>Sapper</a> repos, and join us in the <a href='https://twitter.com/twiterdoge_'>Twitter 推特</a>. Everyone is welcome, especially you!</p>
 		`,
   },
 ];

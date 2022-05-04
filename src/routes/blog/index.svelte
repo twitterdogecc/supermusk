eba2919694f4b421728171a24601467e93f9850c
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-04 18:37:16 +0800

    Update index.svelte

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -17,15 +17,17 @@
 	}
 	
 	body {
-		background-color: #F1E1FF;	
-	}
+                background-color: #CA8EFF;
+		background-image: url(great-success.png);
+		background-repeat:repeat;
+        }
 </style>
 
 <svelte:head>
 	<title>Blog</title>
 </svelte:head>
 
-
+<body>
 <h1>Developer Ecosystem</h1>
 
 <ul>
@@ -37,3 +39,4 @@
 		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
 	{/each}
 </ul>
+</body>

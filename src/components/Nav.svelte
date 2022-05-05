d4bd119097e823ec640278f325f01754c5086f50
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-06 02:14:23 +0800

    Update Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -59,5 +59,6 @@
 		
 		<li><a class='{segment === "ido" ? "selected" : ""}' href='ido'>ido</a></li>
 		<li><a class='{segment === "nft" ? "selected" : ""}' href='nft'>nft</a></li>
+		<li><a class='{segment === "wallet" ? "selected" : ""}' >wallet</a></li>
 	</ul>
 </nav>

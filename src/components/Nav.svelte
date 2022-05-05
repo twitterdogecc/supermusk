a7d5b1e5589249258b13d80fee811b661088173b
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-06 04:28:43 +0800

    Update Nav.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -58,9 +58,7 @@
 		<li><a class='{segment === "ido" ? "selected" : ""}' href='ido'>ido</a></li>
 		<li><a class='{segment === "nft" ? "selected" : ""}' href='nft'>nft</a></li>
 		<li><a class='{segment === "market" ? "selected" : ""}' href= "https://www.binance.com/en/nft/home">market</a></li>
+		<li><a class='{segment === "wallet" ? "selected" : ""} "item-right"' href= "https://metamask.io/">wallet</a></li>
 	</ul>
 	
-	<ul class="item-right">
-		<li><a class='{segment === "wallet" ? "selected" : ""}' href= "https://metamask.io/">wallet</a></li>
-	</ul>
 </nav>

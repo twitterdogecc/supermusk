1def205aa594afe003a36ef69f7b14e1bb4afcd1
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-06 04:53:06 +0800

    Update ido.svelte

--- src/routes/ido.svelte
+++ src/routes/ido.svelte
@@ -47,3 +47,35 @@
 
 <p><a href= "https://www.pinksale.finance/">www.pinksale.finance</a></p>
 <p><strong>0x...?</strong></p>
+
+<div>
+<SCRIPT language=JavaScript>
+tmpDate = new Date();
+date = tmpDate.getDate();
+month= tmpDate.getMonth() + 1 ;
+year= tmpDate.getYear();
+document.write(year);
+document.write("年");
+document.write(month);
+document.write("月");
+document.write(date);
+document.write("日 ");
+myArray=new Array(6);
+myArray[0]="星期日 "
+myArray[1]="星期一 "
+myArray[2]="星期二 "
+myArray[3]="星期三 "
+myArray[4]="星期四 "
+myArray[5]="星期五 "
+myArray[6]="星期六 "
+weekday=tmpDate.getDay();
+if (weekday==0 | weekday==6)
+{
+document.write(myArray[weekday])
+}
+else
+{
+document.write(myArray[weekday])
+}
+</SCRIPT>
+</div>

2ac5ccf02909fa7b523f3fd4032647f748df8853
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-06 00:53:03 +0800

    Create nft.svelte

---
+++ src/routes/nft.svelte
@@ -0,0 +1,45 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+	h1 {
+		font-size: 1.8em;
+		text-transform: uppercase;
+		font-weight: 200;
+		margin: 0 0 0.5em 0;
+	}
+	figure {
+		margin: 0 0 1em 0;
+	}
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+	p {
+		margin: 1em auto;
+	}
+	div {
+		text-align: center;
+		margin: 0 auto;
+	}
+	
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+
+<svelte:head>
+	<title>twitterdoge</title>
+</svelte:head>
+
+<h1>coming soon...</h1>
+
+<figure>
+	<img alt='nft' src='nft.png'>
+	<figcaption>Non-Fungible Token</figcaption>
+</figure>

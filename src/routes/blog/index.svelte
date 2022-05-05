f933739f3f0f611a8f9bcff3f540390ffde15b7d
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-05 22:16:45 +0800

    Update index.svelte

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -43,7 +43,7 @@
 
 <div>
 <video  width="320" height="240" controls>
-  <source src="movie.mp4"  type="video/mp4">
-  <source src="movie.ogg"  type="video/ogg">
+  <source src="movie1.mp4"  type="video/mp4">
+  <source src="movie1.ogg"  type="video/ogg">
 </video>
 </div>

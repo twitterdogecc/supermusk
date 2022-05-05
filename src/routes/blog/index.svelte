5899179de584daeee3ff0b9a741836f6e74990b4
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-06 01:19:21 +0800

    Update index.svelte

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -11,6 +11,13 @@
 </script>
 
 <style>
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 600;
+		margin: 0 0 0.5em 0;
+	}
+	
 	ul {
 	        margin: 0 0 1em 0;
 		line-height: 1.5;
@@ -23,6 +30,11 @@
 		text-align: center;
 		margin: 0 auto;
 	}
+	
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 2em;
+		}
 </style>
 
 <svelte:head>

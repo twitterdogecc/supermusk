97de4703c6fd17156b80af829007413990b05c24
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-07 02:42:58 +0800

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,7 +37,8 @@
 </style>
 
 <svelte:head>
-	<link rel="shortcut icon"type="image/x-icon"href="static/favicon.ico" /><title>Twitter Doge</title>
+	<link rel="shortcut icon"type="image/x-icon" href="favicon.ico" />
+	<title>Twitter Doge</title>
 </svelte:head>
 
 <h1>Twitter Doge!</h1>

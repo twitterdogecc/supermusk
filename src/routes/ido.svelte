31a6598c7f5608a4698037f381c87de655e1f069
twitterdogecc <104855402+twitterdogecc@users.noreply.github.com>
2022-05-07 16:46:00 +0800

    Update ido.svelte

--- src/routes/ido.svelte
+++ src/routes/ido.svelte
@@ -26,6 +26,17 @@
 		margin: 0 auto;
 	}
 	
+	body {
+ 		margin: 0;
+ 		padding: 0;
+ 	}
+ 
+	#three_canvas {
+ 		position: absolute;
+ 		width: 100%;
+ 		height: 100%;
+ 	}
+	
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 2em;
@@ -56,4 +67,58 @@ setInterval("show_time.innerHTML= new Date().toLocaleString()",1000);
 </script>
 </div>
 </div>
+</body>
+
+<body>
+<canvas id="three_canvas"></canvas>
+<script>
+ var renderer, camera, scene, light, object;
+ var width, height;
+ function initRenderer() {
+ width = document.getElementById('three_canvas').clientWidth;
+ height = document.getElementById('three_canvas').clientHeight;
+ renderer = new THREE.WebGLRenderer({
+ canvas: document.getElementById('three_canvas')
+ });
+ renderer.setSize(width, height);
+ renderer.setClearColor(0xFFFFFF, 1.0);
+ }
+ 
+ function initCamera() {
+ camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 1, 1000);
+ camera.position.x = 0;
+ camera.position.y = 0;
+ camera.position.z = 200;
+ camera.up.x = 0;
+ camera.up.y = 1;
+ camera.up.z = 0;
+ camera.lookAt({
+ x: 0,
+ y: 0,
+ z: 0
+ });
+ }
+ function initScene() {
+ scene = new THREE.Scene();
+ }
+ function initObject() {
+ var geometry = new THREE.CubeGeometry(100, 100, 100);
+ object = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial());
+ scene.add(object);
+ }
+ function render() {
+ requestAnimationFrame(render);
+ object.rotation.x += 0.05;
+ object.rotation.y += 0.05;
+ renderer.render(scene, camera);
+ }
+ function threeStart() {
+ initRenderer();
+ initCamera();
+ initScene();
+ initObject();
+ render();
+ }
+ window.onload = threeStart();
+</script>
 </body>
